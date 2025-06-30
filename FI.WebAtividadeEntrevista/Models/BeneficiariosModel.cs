e5fb9437d351c71b9d17a79da31b1ad5e1944bd5
Andre Sernajoto <andre.sernajoto@gmail.com>
2025-06-30 02:44:02 -0300

    feat: criando a model de 'Beneficiários'

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiariosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FI.WebAtividadeEntrevista.Models
+{
+    public class BeneficiariosModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public long IdCliente { get; set; }
+    }
+}
