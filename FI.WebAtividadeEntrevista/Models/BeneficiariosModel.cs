aaeb01629fc8a2b9e9f0abfd204ffb7f36b11d00
Andre Sernajoto <andre.sernajoto@gmail.com>
2025-06-30 02:49:23 -0300

    fix: ajuste no 'DataAnnotations' da model de 'Beneficiarios'

--- FI.WebAtividadeEntrevista/Models/BeneficiariosModel.cs
+++ FI.WebAtividadeEntrevista/Models/BeneficiariosModel.cs
@@ -1,10 +1,19 @@
-﻿namespace FI.WebAtividadeEntrevista.Models
+﻿using FI.WebAtividadeEntrevista.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace FI.WebAtividadeEntrevista.Models
 {
     public class BeneficiariosModel
     {
         public long Id { get; set; }
+        
+        [Required]
         public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "CPF é obrigatório.")]
+        [CpfValidator(ErrorMessage = "CPF informado é inválido.")]
         public string CPF { get; set; }
+        
         public long IdCliente { get; set; }
     }
 }
